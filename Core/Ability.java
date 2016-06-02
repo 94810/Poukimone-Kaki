65d23679247ddf4afffeb62c204b7eae9bc783f4
rylux <ryckaert.guillaume@neuf.fr>
2016-06-02 11:25:34 +0200

    Ajout Types (travail en cours...)

--- Core/Ability.java
+++ Core/Ability.java
@@ -1,9 +1,15 @@
 public class Ability{
     public String name;
+    public String type;
     private int max_pp;//pareil pour les pp
     private int power;
+    private int att_red;
+    private int def_red;
+    private int vit_red;
     public int pp;
 
-    public Ability(String name, int max_pp, int power){
+    public Ability(String name, int max_pp, int power, int att_red, int def_red, int vit_red, String type) {
+        pp=max_pp;
     }
+
 }
---
+++ Core/Types.java
@@ -0,0 +1,70 @@
+enum Type{NORMAL,FEU,EAU,ELECTRIQUE,PLANTE,VOL,COMBAT,SOL,ROCHE,POISON,INSECTE,SPECTRE,PSY,DRAGON,GLACE;
+
+    public float compare(Type att, Type def){
+        switch (att){
+            //TYPE NORMAL
+            case NORMAL:{
+                switch (def){
+                    case ROCHE:{
+                        return 0.5f;
+                    }break;
+                    case SPECTRE:{
+                        return 0.0f;
+                    }break;
+                }
+            }break;
+            //TYPE FEU
+            case FEU:{
+                switch (def){
+                    case PLANTE:
+                    case INSECTE:
+                    case GLACE:
+                        return 2.0f;
+                    break;
+                    case FEU:
+                    case EAU:
+                    case ROCHE:
+                    case DRAGON:
+                        return 0.5f;
+                    break;
+                }
+            }break;
+            //TYPE EAU
+            case EAU:{
+                switch (def){
+                    case FEU:
+                    case ROCHE:
+                    case SOL:
+                        return 2.0f;
+                    break;
+                    case EAU:
+                    case DRAGON:
+                    case PLANTE:
+                        return 0.5f;
+                    break;
+                }
+            }break;
+            //TYPE ELECTRIQUE
+            case ELECTRIQUE:{
+                switch (def){
+                    case EAU:
+                    case VOL:
+                        return 2.0f;
+                    break;
+                    case PLANTE:
+                    case ELECTRIQUE:
+                    case DRAGON:
+                        return 0.5f;
+                    break;
+                }
+            }
+
+        }
+
+
+
+
+    }
+
+
+}
